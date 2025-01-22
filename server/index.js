d24d681bab0ae9e57d7da38155ef6a22f31c0109
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-01-22 11:15:42 +0800

    connect be to fe

--- server/index.js
+++ server/index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Server berjalan dengan Express.js!" });
+  res.json({ message: "Server Sudah Siap :D" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+module.exports = app;
