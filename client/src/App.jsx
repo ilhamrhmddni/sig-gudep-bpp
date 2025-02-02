5dcb8fe46adbac5ad06e54c124370ddf7b85dd0e
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-02-03 02:52:25 +0800

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,17 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import UserPage from "./pages/userPage"; // Pastikan path sesuai
 import EditUserPage from "./pages/editUserPage";
 import AddUserPage from "./pages/addUserPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/users" element={<UserPage />} />
-        <Route path="/edit-user/:id" element={<EditUserPage />} />
-        <Route path="/add-user" element={<AddUserPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/users" element={<UserPage />} />
+      <Route path="/edit-user/:id" element={<EditUserPage />} />
+      <Route path="/add-user" element={<AddUserPage />} />
+    </Routes>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import store from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
