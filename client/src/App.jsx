9b62a7350fc910d5b09fb06697866e9823e6e995
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-02-02 22:33:49 +0800

    test user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UserPage from "/pages/UserPage"; // Pastikan path sesuai
-import EditUserPage from "/pages/EditUserPage"; // Jika ada halaman edit user
-import AddUserPage from "/pages/AddUserPage"; // Jika ada halaman tambah user
+import EditUserPage from "./pages/editUserPage"; // Jika ada halaman edit user
+import AddUserPage from "./pages/addUserPage"; // Jika ada halaman tambah user
 
 function App() {
   return (
@@ -12,7 +12,6 @@ function App() {
         <Route path="/users" exact component={UserPage} />
         <Route path="/edit-user/:id" component={EditUserPage} />
         <Route path="/add-user" component={AddUserPage} />
-        {/* Tambahkan route lainnya sesuai kebutuhan */}
       </Switch>
     </Router>
   );
