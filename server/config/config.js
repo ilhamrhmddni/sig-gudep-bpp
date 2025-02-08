7f6ef73be401de83c05c25ea2e62f682b3891202
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-02-08 10:33:04 +0800

    fix be

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   databaseUrl: process.env.SUPABASE_API_URL,
   dialect: "postgres",
+  dialectModule: require("pg"),
   dialectOptions: {
     ssl: {
       require: false,
