2c5ed4865245367cd531f074ca58d0d3fac8868f
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-02-19 22:45:33 +0800

    fe

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,9 +1,11 @@
+// src/ProtectedRoute.jsx
 import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/" replace />;
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
